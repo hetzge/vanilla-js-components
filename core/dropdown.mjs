c1e25d17282326b460b8b298bb17c9a6615fd310
hetzge <hetzge@gmx.de>
2022-11-05 20:44:08 +0100

    Add select option groups

--- core/dropdown.mjs
+++ core/dropdown.mjs
@@ -3,29 +3,48 @@
 import { BaseComponent } from "./base-component.mjs";
 
 /**
- * @typedef {Object} DropdownOption
+ * @typedef {object} DropdownOptionGroup
+ * @property {string} label
+ * @property {Array<DropdownOption>} options
+ */
+{ }
+
+/**
+ * @typedef {object} DropdownOption
  * @property {string} value
  * @property {string} text
  */
- {}
+{ }
 
 export class Dropdown extends BaseComponent {
   static SELECT_EVENT_KEY = "Dropdown#select";
   constructor() {
-	super();
+    super();
     this.$element = document.createElement("select");
-    this.onChange = () => this.dispatchEvent(Dropdown.SELECT_EVENT_KEY); 
+    this.onChange = () => this.dispatchEvent(Dropdown.SELECT_EVENT_KEY);
   }
   /**
-   * @param {Array<DropdownOption>} options
+   * @param {Array<DropdownOption|DropdownOptionGroup>} options
    */
   set options(options) {
     this.$element.innerHTML = "";
     for (let option of options) {
-      const optionElement = document.createElement("option");
-      optionElement.setAttribute("value", option.value);
-      optionElement.innerText = option.text;
-      this.$element.append(optionElement);
+      if (option["label"] !== undefined) {
+        const $optionGroupElement = document.createElement("optgroup");
+        $optionGroupElement.label = option["label"];
+        for (let groupOption of option["options"]) {
+          const $optionElement = document.createElement("option");
+          $optionElement.setAttribute("value", groupOption["value"]);
+          $optionElement.innerText = groupOption["text"];
+          $optionGroupElement.append($optionElement);
+        }
+        this.$element.append($optionGroupElement);
+      } else {
+        const $optionElement = document.createElement("option");
+        $optionElement.setAttribute("value", option["value"]);
+        $optionElement.innerText = option["text"];
+        this.$element.append($optionElement);
+      }
     }
   }
   /** @type {string} */
