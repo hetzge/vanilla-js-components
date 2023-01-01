215cfb5324f0996d99387504ab2fbde16963c821
hetzge <hetzge@gmx.de>
2023-01-01 16:24:34 +0100

    Prevent button from submit page

--- core/button.mjs
+++ core/button.mjs
@@ -6,6 +6,7 @@ export class Button extends BaseContainer {
   static CLICK_EVENT_KEY = "Button#click";
   constructor() {
     super("button");
+    /** @type {HTMLButtonElement} */ (this.$element).type = "button";
     this.onClick = () => this.dispatchEvent(Button.CLICK_EVENT_KEY);
   }
 }
